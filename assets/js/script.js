8f695578884858b6b0405fa11966a403531f6d1c
Joel Martinez <joemartinez87@hotmail.com>
2021-04-23 15:35:51 -0700

    Merging to the main file. Nav and search bars

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+//api key: 8cfcf83c0b1f43e0967daa90da468529
+
+//search for recipe:
+//returns a list of recipes
+//this only returns id and image
+//https://api.spoonacular.com/recipes/complexSearch?apiKey=8cfcf83c0b1f43e0967daa90da468529&query={name}
+
+//get recipe information
+//given id
+//https://api.spoonacular.com/recipes/{recipe id}/information?apiKey=8cfcf83c0b1f43e0967daa90da468529
+
+
+
+//youtube api key: AIzaSyBwL0yd91L36FMNSfLFcw6VeqIN2PpoYbQ
+//for more info: https://developers.google.com/youtube/v3/docs/search/list
+
+//get youtube videos
+//https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q={search term}&key=AIzaSyBwL0yd91L36FMNSfLFcw6VeqIN2PpoYbQ
+
+//to embed a youtube video
+/*
+<iframe width="420" height="345" src="https://www.youtube.com/embed/{video id}">
+</iframe>
+*/
+
