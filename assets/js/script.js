e3933e50d77ab9d5668d8cdc4a39e854af851382
Robert <robertyeam@gmail.com>
2021-04-22 19:12:40 -0700

    added comments about api info

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+//api key: 8cfcf83c0b1f43e0967daa90da468529
+
+//search for recipe:
+//this only returns id and image
+//https://api.spoonacular.com/recipes/complexSearch?apiKey=8cfcf83c0b1f43e0967daa90da468529&query={name}
+
+//get recipe information
+//given id
+//https://api.spoonacular.com/recipes/{recipe id}/information?apiKey=8cfcf83c0b1f43e0967daa90da468529
+
