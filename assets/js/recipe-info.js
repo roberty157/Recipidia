cfa4eec5a6b60f74b3a3b25938aa1478af458bcc
Joel Martinez <joemartinez87@hotmail.com>
2021-04-27 18:33:34 -0700

    Worked on Javascript, I added the api link to connect youtube. I created a function to run both apis

--- assets/js/recipe-info.js
+++ assets/js/recipe-info.js
@@ -2,23 +2,34 @@
 
 var ingredients = document.getElementById("ingredients");
 var ingredientsList = document.getElementById("listIngredients");
+//made these variables global 
+var cookInstructions = document.getElementById("cookInstructions");
+var instructions = document.getElementById("instructions");
 
+//Created a function that wraps the fetch request so that it runs when we click the search button
+function runAPI(event) {
+  event.preventDefault();
+console.log("hello")
+
+var searchTerm=document.querySelector(".searchInputBox").value //created this element to grab the users search
+var requestUrl= "https://api.spoonacular.com/recipes/complexSearch?apiKey=8cfcf83c0b1f43e0967daa90da468529&query="+searchTerm
 fetch(requestUrl)
 
 .then(function (response) {
   return response.json();
 })
 .then(function (data) {
-  for (var i = 0; i < data.length; i++) {
+  // var data=res.result
+  console.log(data)
+  for (var i = 0; i < data.results.length; i++) {
     var ingredientsList = document.createElement('li');
-    ingredientsList.textContent = data[i];
-    ingredients.appendChild(listItem);
+    ingredientsList.textContent = data.results[i];
+    ingredients.appendChild(ingredientsList);//Changed list item to ingredients list to append. and list item doesnt exist
   }
 });
 
 ////////////////////
-var cookInstructions = document.getElementById("cookInstructions");
-var instructions = document.getElementById("instructions");
+
 
 fetch(requestUrl)
 
@@ -26,14 +37,16 @@ fetch(requestUrl)
   return response.json();
 })
 .then(function (data) {
-  for (var i = 0; i < data.length; i++) {
+  // var data=res.result
+  console.log(data)
+  for (var i = 0; i < data.results.length; i++) {
     var instructions = document.createElement('li');
-    instructions.textContent = data[i];
-    cookInstructions.appendChild(listItem);
+    instructions.textContent = data.results[i].title;
+    cookInstructions.appendChild(instructions);// changed to instuctions to append
   }
 });
 //youtube api
-var requestURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q={search term}&key=AIzaSyBwL0yd91L36FMNSfLFcw6VeqIN2PpoYbQ"
+var requestURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q="+searchTerm+"_cooking"+"&key=AIzaSyBwL0yd91L36FMNSfLFcw6VeqIN2PpoYbQ"
 var iframe= document.getElementById("iframe")
 
 var apiKey ="AIzaSyBwL0yd91L36FMNSfLFcw6VeqIN2PpoYbQ"
@@ -41,11 +54,15 @@ var apiKey ="AIzaSyBwL0yd91L36FMNSfLFcw6VeqIN2PpoYbQ"
 fetch(requestURL)
 .then(function (response) {
   return response.json();
-});
-const getSearchTerm = () => searchTerms[Math.floor(Math.random() * (searchTerms.length-1))];
+}).then(function(data){
+  console.log(data)
+  iframe.setAttribute("src","https://www.youtube.com/embed/"+data.items[0].id.videoId)
+  // const getSearchTerm = () => searchTerms[Math.floor(Math.random() * (searchTerms.length-1))];
+})
 
+}
 
-
+document.getElementById("searchBtn").onclick=runAPI
 
 
 
