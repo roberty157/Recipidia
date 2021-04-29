0709c10473768bc5d01e80e6e5dca6c3aed8ff75
maythingo <maythingo@aim.com>
2021-04-29 14:56:29 -0700

    working of javascript for result page, still in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,29 @@
 </iframe>
 */
 
+var searchTerm = document.getElementById("search-term");
+var resultLinks = document.getElementById("sourcelink");
+
+function runAPI(event) {
+    event.preventDefault();
+  console.log("hello")
+  
+  var searchTerm=document.querySelector(".searchInputBox").value //created this element to grab the users search
+  var requestUrl= "https://api.spoonacular.com/recipes/findByIngredients?apiKey=8cfcf83c0b1f43e0967daa90da468529&query=" + searchTerm
+  
+  fetch(requestUrl)
+  
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    // var data=res.result
+    console.log(data)
+    for (var i = 0; i < data.results; i++) {
+      var resultLinks = document.createElement('li');
+      resultLinks.textContent = data.results[i];
+      searchTerm.appendChild(resultLinks);
+    }
+  })};
+
+document.getElementById("searchBtn").onclick=runAPI;
